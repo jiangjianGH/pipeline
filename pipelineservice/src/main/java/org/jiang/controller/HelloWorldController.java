faad936c0533d7f00629080f1cd28f2b7af3c279
Jiangjian (Jack Jiang <j00160659@china.huawei.com>
2016-10-23 23:39:30 +0800

    Committer: Jiangjian (Jack Jiang <j00160659@china.huawei.com>  On branch master 	new file:   pipelineservice/src/main/java/org/jiang/controller/BookServiceController (2).java 	modified:   pipelineservice/src/main/java/org/jiang/controller/HelloWorldController.java 	new file:   pipelineservice/src/main/java/org/jiang/controller/HelloWorldWithoutReturnModelAndViewController.java 	new file:   pipelineservice/src/main/resources/spring-common-config.xml 	new file:   pipelineservice/src/main/resources/spring-dao-config.xml

--- pipelineservice/src/main/java/org/jiang/controller/HelloWorldController.java
+++ pipelineservice/src/main/java/org/jiang/controller/HelloWorldController.java
@@ -1,5 +1,11 @@
 package org.jiang.controller;
 
+import java.io.BufferedReader;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -8,7 +14,43 @@ import org.springframework.web.servlet.mvc.AbstractController;
 
 public class HelloWorldController extends AbstractController {
 	@Override
-    protected ModelAndView handleRequestInternal(HttpServletRequest req, HttpServletResponse resp) throws Exception {  
+    protected ModelAndView handleRequestInternal(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+		Map map = new HashMap();  
+        Enumeration paramNames = req.getParameterNames();  
+        while (paramNames.hasMoreElements()) {  
+            String paramName = (String) paramNames.nextElement();  
+  
+            String[] paramValues = req.getParameterValues(paramName);  
+            if (paramValues.length == 1) {  
+                String paramValue = paramValues[0];  
+                if (paramValue.length() != 0) {  
+                    map.put(paramName, paramValue);  
+                }  
+            }  
+        }
+        req.setCharacterEncoding("UTF-8");
+		StringBuffer jb = new StringBuffer();
+		String line = null;
+		try
+		{
+			BufferedReader reader = req.getReader();
+			while ((line = reader.readLine()) != null)
+				jb.append(line);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		System.out.println(jb);
+		
+  
+        Set<Map.Entry<String, String>> set = map.entrySet();  
+        System.out.println("------------------------------");  
+        for (Map.Entry entry : set) {  
+            System.out.println(entry.getKey() + ":" + entry.getValue());  
+        }  
+        System.out.println("------------------------------");
+        
        //1、收集参数、验证参数  
        //2、绑定参数到命令对象  
        //3、将命令对象传入业务对象进行业务处理  
